a48b77a19f058b54cd2c44352f3d5ab39ec26bfd
Junejie Ruzol <junejie.ruzol@zumtobelgroup.com>
2017-07-06 00:20:29 +0800

    add urls

--- src/trending/trending/spiders/github.py
+++ src/trending/trending/spiders/github.py
@@ -5,7 +5,20 @@ import scrapy
 class GithubSpider(scrapy.Spider):
     name = 'github'
     allowed_domains = ['github.com']
-    start_urls = ['http://github.com/']
+    start_urls = [
+        'https://github.com/showcases/programming-languages',
+        'https://github.com/showcases/web-accessibility',
+        'https://github.com/showcases/open-source-integrations',
+        'https://github.com/showcases/text-editors',
+        'https://github.com/showcases/devops-tools',
+        'https://github.com/showcases/serverless-architecture',
+        'https://github.com/showcases/productivity-tools',
+        'https://github.com/showcases/projects-that-power-github',
+        'https://github.com/showcases/data-visualization',
+        'https://github.com/showcases/machine-learning',
+        'https://github.com/showcases/nosql-databases'
+    ]
 
     def parse(self, response):
-        pass
+        r = response.css('h1.f00-light.lh-condensed.mb-1::text')
+        print(r)
