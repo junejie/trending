366e32822a04ce45c850056faf25f3ebfb6cab42
Junejie Ruzol <junejie.ruzol@zumtobelgroup.com>
2017-07-12 00:11:29 +0800

    readd link

--- src/trending/trending/spiders/github.py
+++ src/trending/trending/spiders/github.py
@@ -8,18 +8,17 @@ class GithubSpider(scrapy.Spider):
     allowed_domains = ['github.com']
     start_urls = [
         'https://github.com/showcases/programming-languages',
-        'https://github.com/showcases/web-accessibility'
+        'https://github.com/showcases/web-accessibility',
+        'https://github.com/showcases/open-source-integrations',
+        'https://github.com/showcases/text-editors',
+        'https://github.com/showcases/devops-tools',
+        'https://github.com/showcases/serverless-architecture',
+        'https://github.com/showcases/productivity-tools',
+        'https://github.com/showcases/projects-that-power-github',
+        'https://github.com/showcases/data-visualization',
+        'https://github.com/showcases/machine-learning',
+        'https://github.com/showcases/nosql-databases'
     ]
-
-        # 'https://github.com/showcases/open-source-integrations',
-        # 'https://github.com/showcases/text-editors',
-        # 'https://github.com/showcases/devops-tools',
-        # 'https://github.com/showcases/serverless-architecture',
-        # 'https://github.com/showcases/productivity-tools',
-        # 'https://github.com/showcases/projects-that-power-github',
-        # 'https://github.com/showcases/data-visualization',
-        # 'https://github.com/showcases/machine-learning',
-        # 'https://github.com/showcases/nosql-databases'
 
     def parse(self, response):
         items = TrendingItem()
